cb75ca4af402074df6816946e1ae1ea38e5f697f
eliftanrverdii <61614868+eliftanrverdii@users.noreply.github.com>
2020-09-09 13:36:29 +0300

    Add files via upload

--- CODE/L298N_water_pump.py
+++ CODE/L298N_water_pump.py
@@ -2,27 +2,29 @@ import RPi.GPIO as gpio
 import time
 
 def init():
- gpio.setmode(gpio.BCM)
- gpio.setup(7, gpio.OUT)
- gpio.setup(8, gpio.OUT)
- gpio.setup(19, gpio.OUT)
- gpio.setup(25, gpio.OUT)
+ gpio.setmode(gpio.BOARD)
+ gpio.setup(26, gpio.OUT)
+ gpio.setup(24, gpio.OUT)
+ gpio.setup(35, gpio.OUT)
+ gpio.setup(22, gpio.OUT)
 
 def forward(sec):
  init()
- gpio.output(7, True)
- gpio.output(8, False)
- gpio.output(19, True)
- gpio.output(25, False)
+ gpio.setup(26, gpio.OUT)
+ gpio.setup(24, gpio.OUT)
+ gpio.setup(35, gpio.OUT)
+ gpio.setup(22, gpio.OUT)
+
  time.sleep(sec)
  gpio.cleanup()
 
 def reverse(sec):
  init()
- gpio.output(7, False)
- gpio.output(8, True)
- gpio.output(19, False)
- gpio.output(25, True)
+ gpio.setup(26, gpio.OUT)
+ gpio.setup(24, gpio.OUT)
+ gpio.setup(35, gpio.OUT)
+ gpio.setup(22, gpio.OUT)
+
  time.sleep(sec)
  gpio.cleanup()
 
