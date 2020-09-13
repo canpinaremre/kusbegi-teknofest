c1b4e4a1746adc0f7d021967398328ce6070d24a
a-altug <39158671+a-altug@users.noreply.github.com>
2020-09-13 12:41:41 +0300

    Create g2_test_cam.py
    can not pass thread.stop() and break while situation

---
+++ CODE/UNTESTED/g2_test_cam.py
@@ -0,0 +1,25 @@
+from Test_Target_Red_Thread import CSI_Camera
+import threading
+import cv2
+
+cam = CSI_Camera()
+cam.open()
+cam.start()
+cam.start_counting_fps()
+while True:
+
+	print("main", cam.con_area)
+	print("errX", cam.errX)
+	print("errY", cam.errY)
+	if cam.frame is not None:
+		
+	cv2.imshow("frame", cam.frame)
+	"""
+	if (cv2.waitKey(5) & 0xFF) == 27:
+    	break
+	"""
+"""
+finally:
+    cam.stop()
+    cam.release()
+"""
