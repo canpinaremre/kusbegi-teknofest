3a5b964b7bb90e69034b0856ff21362123db9881
pagex <emrecanpinar06@gmail.com>
2020-09-15 17:19:16 +0300

    kamera test

---
+++ CODE/UNTESTED/kamera_test.py
@@ -0,0 +1,27 @@
+from Test_Target_Red_Thread import CSI_Camera
+
+
+
+cam = CSI_Camera()
+cam.open()
+cam.start()
+
+
+
+while cam.running is False:
+    print("wait cam")
+    sleep(0.5)
+
+sleep(0.31)
+
+max_area = 0
+
+while True:
+    if not False:            
+            if (cam.con_area > max_area):
+                max_area = cam.con_area
+                print("daha büyük alan algılandı")
+                print(max_area)
+    else:
+        break
+
