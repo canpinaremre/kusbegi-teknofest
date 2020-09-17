da5b1ea84db80a89f29eb02b3b87a2d0892e002b
pagex <emrecanpinar06@gmail.com>
2020-09-17 17:28:23 +0300

    pump in and out test

--- CODE/SRC/mission-2/pump_test.py
+++ CODE/SRC/mission-2/pump_test.py
@@ -5,7 +5,7 @@ pump = Pump()
 
 sens = WaterLevelSensor()
 
-print("Start pumping")
+print("Start pumping in")
 #drone.log.logger("Start pumping")
 
 
@@ -18,7 +18,9 @@ while ( (time()- ts) < MISSION_PUMP_TIMEOUT ):
     pump.pump_water_in()
     sleep(0.5)
     print("pumping in")
-    if (sens.readadc(sens.photo_ch, sens.SPICLK, sens.SPIMOSI, sens.SPIMISO, sens.SPICS) > MISSION_MAX_WATER_SENSOR):
+    sens_value = sens.readadc(sens.photo_ch, sens.SPICLK, sens.SPIMOSI, sens.SPIMISO, sens.SPICS)
+    print(sens_value)
+    if (sens_value > MISSION_MAX_WATER_SENSOR):
         print("Water level reached!")
         #drone.log.logger("Water level reached!")
         break
@@ -29,3 +31,19 @@ print("Stop pumping")
 pump.close_and_clean()
 sens.close_and_clean()
 
+pump = Pump()
+sens = WaterLevelSensor()
+
+print("Start pumping out")
+
+ts = time()
+
+while ( (time()- ts) < MISSION_PUMP_TIMEOUT ):
+    pump.pump_water_out()
+    sleep(0.5)
+    print("pumping in")
+    sens_value = sens.readadc(sens.photo_ch, sens.SPICLK, sens.SPIMOSI, sens.SPIMISO, sens.SPICS)
+    print(sens_value)
+    
+
+print("Stop pumping")
